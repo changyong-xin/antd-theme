7f467aa07eb3260dd761b789b5e4b6d848423768
changyong-xin <103397386+changyong-xin@users.noreply.github.com>
2023-09-02 11:18:13 +0800

    init-xin

---
+++ src/antdComponents.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd';
+function AntdComponets() {
+    return (
+        <div>
+            <Button type='primary'>测试 </Button>
+        </div>
+    );
+}
+
+export default AntdComponets;
